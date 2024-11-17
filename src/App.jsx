14ddb821722d44b8864d2d527b6b5048f6831524
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-11-17 11:32:48 -0300

    07_hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 //import HookState02 from "./components/UseState_02"
 //import HookState03 from "./components/UseState_03"
 //import HookState04 from "./components/UseState_04"
-import HookState06 from "./components/UseState_06"
+import HookState07 from "./components/UseState_07"
 
 
 
@@ -18,7 +18,8 @@ const App = () => {
           {/*<HookState03 /> */}
           {/*<HookState04 /> */}
           {/*<HookState05 /> */}
-          <HookState06 />
+          {/*<HookState06 /> */}
+          <HookState07 />
         </>
     )
 }
