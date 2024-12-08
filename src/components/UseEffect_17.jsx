506325c9eb97287273acd7413824a934441eaad7
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-12-08 19:42:10 -0300

    17_hooks

--- src/components/UseEffect_17.jsx
+++ src/components/UseEffect_17.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect } from "react";
 const HookEffect17 = ()=> {
 
     const [valor, setValor] = useState(0) 
+    const [nome, setNome] = useState('')
 
     function incremento() {
         setValor(valor  + 1)
@@ -12,11 +13,29 @@ const HookEffect17 = ()=> {
         console.log('componente iniciado')
     },[])
 
+    useEffect(()=> {
+        console.log('iniciar quando o valor for alterado')
+    },[valor])
+
+    useEffect(()=> {
+        console.log('quando o nome for inserido')
+    },[nome])
+
+    useEffect(()=> {
+        console.log('quando qualquer um dos dois for clicado')
+    },[valor, nome])
+
+    function alterarNome() {
+        setNome('Alex Feitoza')
+    }
+
         return(
             <>
                 <h3>UseEffect 01</h3>
                 <button onClick={incremento}>Incremento</button>
                 <h3>valor:{valor}</h3>
+                <button onClick={alterarNome}>Alterar Nome</button>
+                <p>{nome}</p>
             </>
         )
 }
